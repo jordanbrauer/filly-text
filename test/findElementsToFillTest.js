5e3737142f0eb3ef08f087b21b998fbb1289af0c
jordanbrauer <j.brauer@hotmail.com>
2016-10-23 17:47:31 -0500

    Starting test for findElementsToFill function.

---
+++ test/findElementsToFillTest.js
@@ -0,0 +1,9 @@
+// var chai               = require('chai'),
+//     assert             = chai.assert,
+//     findElementsToFill = require('../js/findElementsToFill.js').findElementsToFill;
+var assert = chai.assert;
+
+describe('findElementsToFill', function() {
+  
+  it('should return an object of each element found');
+});
