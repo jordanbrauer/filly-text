180e30cf3665d8c3b8ff2b7295a47248497b4098
jordanbrauer <jbrauer.inc@gmail.com>
2016-10-30 19:48:32 -0500

    Added gulp to project devdeps.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+// babel es2015
+gulp.task('babel', () => {
+  return gulp.src('./js/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+// build
+gulp.task('build', ['babel']);
+
+// watch task
+gulp.task('watch', ['build'], () => {
+  gulp.watch('./js/*', ['babel']);
+});
+
+gulp.task('default', () => {
+  // ...
+});
