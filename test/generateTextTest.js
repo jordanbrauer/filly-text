d0e14affbe7010eda5751179de381152bb72e214
jordanbrauer <jbrauer.inc@gmail.com>
2016-10-30 23:21:11 -0500

    Added generateText method and test. Passing first test case.

---
+++ test/generateTextTest.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('generateText', () => {
+  // test case 1
+  it('should return a string with a length greater than 0', () => {
+    let dataFillyText = findElementsByAttribute('data-fillytext');
+
+    dataFillyText.forEach(generateText);
+
+    expect(dataFillyText[0].innerHTML.length).to.be.above(0);
+    expect(dataFillyText[1].innerHTML.length).to.be.above(0);
+    expect(dataFillyText[2].innerHTML.length).to.be.above(0);
+    expect(dataFillyText[3].innerHTML.length).to.be.above(0);
+    expect(dataFillyText[4].innerHTML.length).to.be.above(0);
+    expect(dataFillyText[5].innerHTML.length).to.be.above(0);
+  });
+});
