65c4bef17231c2bab86cd7e54846c7b9283eaff3
jordanbrauer <jbrauer.inc@gmail.com>
2016-10-30 20:08:48 -0500

    findElementsByAttribute.js complete for now, passing all tests.

--- js/findElementsByAttribute.js
+++ js/findElementsByAttribute.js
@@ -4,9 +4,11 @@ const findElementsByAttribute = (attribute) => {
   let nodeList = document.querySelectorAll('[' + attribute + ']');
   let nodeArray = [];
 
+  // loop through the node list that is generated
   for (let i = 0; i < nodeList.length; i++) {
-    nodeArray.push(nodeList[i]);
+    nodeArray.push(nodeList[i]); // push each found elemnt into an array
   }
 
+  console.log(nodeArray);
   return nodeArray;
 }
--- test/findElementsByAttributeTest.js
+++ test/findElementsByAttributeTest.js
@@ -1,17 +1,18 @@
 'use strict';
 
-const expect = chai.expect;
-
 describe('findElementsToFill', () => {
+  // test case 1
   it('should return an array of elements within the body', () => {
-    let foundElements = new findElementsByAttribute();
+    let foundElements = findElementsByAttribute();
 
     expect(foundElements).to.be.a('array');
+    expect(foundElements).to.not.be.a('object');
     expect(foundElements).to.not.be.a('string');
   });
 
+  // test case 2
   it('should not contain elements without the specified selector', () => {
-    let foundElements = new findElementsByAttribute('data-fillytext');
+    let foundElements = findElementsByAttribute('data-fillytext');
 
     expect(foundElements[0].hasAttribute('data-fillytext')).to.equal(true);
     expect(foundElements[1].hasAttribute('data-fillytext')).to.equal(true);
