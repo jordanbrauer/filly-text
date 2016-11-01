bc9caa4df6088e481c8e64daf5afbaf3f8d29505
jordanbrauer <jbrauer.inc@gmail.com>
2016-10-31 22:15:56 -0500

    Added del to deps for clean task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
-'use strict';
-
+// plugins
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const del = require('del');
+
+// clean
+gulp.task('clean', () => {
+  const stream = del('./dist/**/*.js');
+
+  return stream;
+});
 
 // babel es2015
-gulp.task('babel', () => {
-  return gulp.src('./js/**/*.js')
-    .pipe(babel({
-      presets: ['es2015']
-    }))
+gulp.task('babel', ['clean'], () => {
+  const stream = gulp.src('./js/**/*.js')
+    .pipe(babel({ presets: ['es2015'] }))
     .pipe(gulp.dest('./dist'));
+
+  return stream;
 });
 
 // build
@@ -17,9 +24,10 @@ gulp.task('build', ['babel']);
 
 // watch task
 gulp.task('watch', ['build'], () => {
-  gulp.watch('./js/*', ['babel']);
+  gulp.watch('./js/*', ['build']);
 });
 
+// default
 gulp.task('default', () => {
-  // ...
+  console.log('See readme or gulpfile for commands!');
 });
